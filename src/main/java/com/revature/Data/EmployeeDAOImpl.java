95e9048a67c78a220ed86f7fbf541476bd0c3b66
Ian Mitchell <ian.mitchell@revature.net>
2021-11-19 10:25:09 -0700

    EmployeeDAOImpl done

--- src/main/java/com/revature/Data/EmployeeDAOImpl.java
+++ src/main/java/com/revature/Data/EmployeeDAOImpl.java
@@ -3,6 +3,7 @@ package com.revature.Data;
 import com.revature.Classes.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class EmployeeDAOImpl implements EmployeeDAO{
@@ -17,16 +18,36 @@ public class EmployeeDAOImpl implements EmployeeDAO{
 
     @Override
     public Employee getEmployee(int id) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Employee employee = session.get(Employee.class, id);
+
+        transaction.commit();
+        session.close();
+
+        return employee;
     }
 
     @Override
     public void saveEmployee(Employee employee) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+        session.save(employee);
+
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void updateEmployee(Employee employee) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
 
+        session.update(employee);
+
+        transaction.commit();
+        session.close();
     }
 }
