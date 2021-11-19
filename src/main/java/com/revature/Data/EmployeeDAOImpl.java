60e3d15c06ca570dbbe26724c0e7bb65853705c2
Ian Mitchell <ian.mitchell@revature.net>
2021-11-19 10:19:07 -0700

    Setup EmployeeDAOImpl constructor

--- src/main/java/com/revature/Data/EmployeeDAOImpl.java
+++ src/main/java/com/revature/Data/EmployeeDAOImpl.java
@@ -1,8 +1,19 @@
 package com.revature.Data;
 
 import com.revature.Classes.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class EmployeeDAOImpl implements EmployeeDAO{
+    private Configuration configuration;
+    private SessionFactory sessionFactory;
+
+    public EmployeeDAOImpl() {
+        configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        sessionFactory = configuration.buildSessionFactory();
+    }
 
     @Override
     public Employee getEmployee(int id) {
