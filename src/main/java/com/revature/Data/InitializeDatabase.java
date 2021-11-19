31d66237b4d962673a032b290ade5b63f9414ad1
Ian Mitchell <ian.mitchell@revature.net>
2021-11-19 14:53:53 -0700

    Dummy Expense data

--- src/main/java/com/revature/Data/InitializeDatabase.java
+++ src/main/java/com/revature/Data/InitializeDatabase.java
@@ -1,6 +1,7 @@
 package com.revature.Data;
 
 import com.revature.Classes.Employee;
+import com.revature.Classes.Expense;
 import com.revature.Classes.Manager;
 import com.revature.Classes.Permissions;
 
@@ -10,6 +11,8 @@ import java.util.List;
 public class InitializeDatabase {
     public static void main(String[] args) {
         EmployeeDAO employeeDAO = DaoFactory.getEmployeeDAO();
+        ExpenseDAO expenseDAO = DaoFactory.getExpenseDAO();
+
         List<Employee> employeeList = new ArrayList<>();
 
         employeeList.add(new Employee("Peter Linz", "ernie", "password"));
@@ -26,6 +29,61 @@ public class InitializeDatabase {
             employeeDAO.saveEmployee(employee);
         }
 
+        employeeList = employeeDAO.getEmployees();
+        List<Expense> expenseList = new ArrayList<>();
 
+        //create expenses to be approved
+        for (Employee employee : employeeList){
+            expenseList.add(new Expense(123.45, "For Stuff", employee));
+            expenseList.add(new Expense(643.21, "Duck Food", employee));
+        }
+
+        for (int i = 0; i < expenseList.size(); i++) {
+            if (expenseList.size() % (i + 1) == 0){
+                expenseList.get(i).approveExpense((Manager) employeeDAO.getEmployee(2), "Sure thing");
+            } else if (expenseList.size() % (i + 1) == 1) {
+                expenseList.get(i).approveExpense((Manager) employeeDAO.getEmployee(8), "Sure thing");
+            } else if (expenseList.size() % (i + 1) == 2) {
+                expenseList.get(i).approveExpense((Manager) employeeDAO.getEmployee(9), "Sure thing");
+            }
+        }
+
+        for (Expense expense : expenseList){
+            expenseDAO.saveExpense(expense);
+        }
+
+        expenseList.clear();
+
+        //Create expenses to be Denied
+        for (Employee employee : employeeList){
+            expenseList.add(new Expense(999.99, "New Leather Jacket", employee));
+            expenseList.add(new Expense(1.25, "Duck Burial", employee));
+        }
+
+        for (int i = 0; i < expenseList.size(); i++) {
+            if (i <= expenseList.size() / 3){
+                expenseList.get(i).denyExpense((Manager) employeeDAO.getEmployee(2), "Umm... NO!");
+            } else if (i > expenseList.size() / 3 && i <= 2 * expenseList.size() / 3) {
+                expenseList.get(i).denyExpense((Manager) employeeDAO.getEmployee(8), "Umm... NO!");
+            } else if (i > 2 * expenseList.size() / 3) {
+                expenseList.get(i).denyExpense((Manager) employeeDAO.getEmployee(9), "Umm... NO!");
+            }
+        }
+
+        for (Expense expense : expenseList){
+            expenseDAO.saveExpense(expense);
+        }
+
+        expenseList.clear();
+
+        //create expenses to leave as pending
+        for (Employee employee : employeeList){
+            expenseList.add(new Expense(999.99, "New Leather Jacket", employee));
+            expenseList.add(new Expense(1.25, "Duck Burial", employee));
+        }
+
+        for (Expense expense : expenseList){
+            expenseDAO.saveExpense(expense);
+        }
     }
 }
