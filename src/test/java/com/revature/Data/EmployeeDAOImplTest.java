1320fd565863800a21cb20d78b62cbd2170b5fbf
Ian Mitchell <ian.mitchell@revature.net>
2021-11-19 17:27:55 -0700

    EmployeeDAOImplTest complete

--- src/test/java/com/revature/Data/EmployeeDAOImplTest.java
+++ src/test/java/com/revature/Data/EmployeeDAOImplTest.java
@@ -1,43 +1,88 @@
 package com.revature.Data;
 
 import com.revature.Classes.Employee;
-import org.junit.After;
+import com.revature.Classes.Manager;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeDAOImplTest {
-    private EmployeeDAO employeeDAO;
-
-    @Before
-    public void setUp() throws Exception {
-        employeeDAO = DaoFactory.getEmployeeDAO();
-    }
 
     @Test
-    @Transactional
     public void saveEmployee() {
+        EmployeeDAO employeeDAO = DaoFactory.getEmployeeDAO();
+
         Employee employee = new Employee("Peter Linz", "ernie", "password");
         employeeDAO.saveEmployee(employee);
 
         Employee testEmployee = employeeDAO.getEmployee(1);
 
         Assert.assertEquals(employee.getUsername(), testEmployee.getUsername());
+        employeeDAO.close();
     }
 
     @Test
-    @Transactional
     public void updateEmployee() {
+        EmployeeDAO employeeDAO = DaoFactory.getEmployeeDAO();
+
         Employee employee = new Employee("Peter Linz", "ernie", "password");
         employeeDAO.saveEmployee(employee);
 
         employee.setUsername("aslkdfjklsdjf");
         employeeDAO.updateEmployee(employee);
 
-        Employee testEmployee = employeeDAO.getEmployee(1);
+        Employee employeeTest = employeeDAO.getEmployee(1);
 
-        Assert.assertEquals(employee.getUsername(), testEmployee.getUsername());
+        Assert.assertEquals(employee.getUsername(), employeeTest.getUsername());
+        employeeDAO.close();
+    }
+
+    @Test
+    public void getEmployeeById() {
+        EmployeeDAO employeeDAO = DaoFactory.getEmployeeDAO();
+
+        Employee employee = new Employee("Peter Linz", "ernie", "password");
+        employeeDAO.saveEmployee(employee);
+
+        Employee employeeTest = employeeDAO.getEmployee(employee.getId());
+
+        Assert.assertEquals(employee.getName(), employeeTest.getName());
+        employeeDAO.close();
+    }
+
+    @Test
+    public void GetEmployeeByUsername() {
+        EmployeeDAO employeeDAO = DaoFactory.getEmployeeDAO();
+
+        Employee employee = new Employee("Peter Linz", "ernie", "password");
+        employeeDAO.saveEmployee(employee);
+
+        Employee employeeTest = employeeDAO.getEmployee(employee.getUsername());
+
+        Assert.assertEquals(employee.getName(), employeeTest.getName());
+        employeeDAO.close();
+    }
+
+    @Test
+    public void getEmployees() {
+        EmployeeDAO employeeDAO = DaoFactory.getEmployeeDAO();
+
+        List<Employee> employeeList = new ArrayList<>();
+
+        employeeList.add(new Employee("Peter Linz", "ernie", "password"));
+        employeeList.add(new Manager("Eric Jacobson", "bert", "password"));
+
+        for (Employee employee : employeeList) {
+            employeeDAO.saveEmployee(employee);
+        }
+
+        List<Employee> employeeListTest = employeeDAO.getEmployees();
+
+        Assert.assertEquals(employeeList.get(0).getUsername(), employeeListTest.get(0).getUsername());
+        Assert.assertEquals(employeeList.get(1).getUsername(), employeeListTest.get(1).getUsername());
+        employeeDAO.close();
     }
 }
